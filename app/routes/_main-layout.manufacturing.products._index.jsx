f67ca8a99b2e652da48d86677a32cfef41f8ae45
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-11-15 23:37:24 +0700

    feat(products): update index products view

--- app/routes/_main-layout.manufacturing.products._index.jsx
+++ app/routes/_main-layout.manufacturing.products._index.jsx
@@ -85,7 +85,7 @@ export default function Products() {
             {products.length > 0 && (
               <Link
                 to="/manufacturing/products/add"
-                className="text-gray-900 bg-white gap-2 w-full md:w-fit hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+                className="text-gray-900 bg-white gap-2 w-full md:w-fit hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center justify-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
               >
                 <Plus size={16} weight="bold" />
                 New
