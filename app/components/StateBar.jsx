5fc3811b50d00230e05e0ff4da8d35ac28c37363
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-17 00:03:59 +0700

    style(StateBar): change text color percent

--- app/components/StateBar.jsx
+++ app/components/StateBar.jsx
@@ -2,7 +2,7 @@ const StateBar = ({ data, status, total }) => {
   const percentage = (data / total) * 100;
   return (
     <div className="flex flex-row gap-4 items-center min-w-48">
-      <p className="text-[15px] text-gray-700 w-10">
+      <p className="text-[15px] text-gray-700 dark:text-gray-400 w-10">
         <span className="font-medium">{percentage}</span>%
       </p>
       <div className="w-2/3 h-2 rounded-full overflow-hidden bg-gray-100 dark:bg-gray-700">
