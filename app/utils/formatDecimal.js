6ef78e8230e1215ae4f9718a38fe6ca953fd3a78
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2024-12-16 21:23:12 +0700

    fix(format decimal): fixing decimal

--- app/utils/formatDecimal.js
+++ app/utils/formatDecimal.js
@@ -1,6 +1,17 @@
 const formatToDecimal = (value) => {
   const number = parseFloat(value);
   if (isNaN(number)) return "Input harus berupa angka.";
+
+  // konversi value menjadi string
+  const valueStr = value.toString();
+  const decimalPlaces = valueStr.includes(".")
+    ? valueStr.split(".")[1].length
+    : 0;
+
+  // jika jumlah desimal lebih dari 2, show sesuai input, jika tidak, format ke 2 desimal
+  if (decimalPlaces > 2) {
+    return number.toString();
+  }
   return number.toFixed(2);
 };
 
